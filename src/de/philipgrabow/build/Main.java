6628a7d7de8e276b0c1531dbb0869df36f36dda4
BuildTools <unconfigured@null.spigotmc.org>
2017-03-11 12:43:29 +0100

    Version 1.9.0.9 AutoRank delete

--- src/de/philipgrabow/build/Main.java
+++ src/de/philipgrabow/build/Main.java
@@ -412,7 +412,6 @@ public class Main extends JavaPlugin implements Listener ,CommandExecutor{
 		String KeepXPDeathMessage = "BCP.Death.KeepXP.DeathMessage";
 		getConfig().addDefault(KeepXPDeathMessage, "&2 %p% &bdu bist gestorben hast aber deine XP-Level behalten!!");
 		
-		getConfig().addDefault("AutoRank.Enabled", false);
 		//
 		FileConfiguration cfg = this.getConfig();
 		cfg.options().copyDefaults(true);
